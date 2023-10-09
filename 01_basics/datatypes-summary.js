db6153c5b9ce5df3d3d0fca6d5de7e813297e211
Satish Kumar Gupta <37590413+satishgupta07@users.noreply.github.com>
2023-10-09 10:57:09 +0000

    Stack and Heap Memory in JavaScript

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,31 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+/* ****************************************************************************************** */
+// Stack (Primitive) - We get a copy of variable
+// Heap (Non-Primitive) - We get a reference of original value - So when we change anything it will 
+//                        reflect in original Value
+
+let myYouTubeName = "hiteshchoudharydotcom"
+
+let anotherName = myYouTubeName
+anotherName = "chaiaurcode"
+
+console.log(myYouTubeName)
+console.log(anotherName)
+
+let userOne = {
+    email : "user@gmail.com",
+    upi : "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JavaScript
 A code repo for JavaScript
+
+![Stack and Heap Memory in JavaScript](<Screenshot 2023-10-09 162222.png>)
