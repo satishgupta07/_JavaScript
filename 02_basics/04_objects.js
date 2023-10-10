b3d6256933c8254dbfeef699189ad9e3fa22fd5c
Satish Kumar Gupta <37590413+satishgupta07@users.noreply.github.com>
2023-10-10 13:55:22 +0000

    Object de-structuring

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -53,3 +53,16 @@ console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
