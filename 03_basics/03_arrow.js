f5e513d93807aaec2ab15c163920a95383d9fe19
Satish Kumar Gupta <37590413+satishgupta07@users.noreply.github.com>
2023-10-13 07:30:06 +0000

    this and arrow function

---
+++ 03_basics/03_arrow.js
@@ -0,0 +1,48 @@
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()
+
+console.log(this);
+
+console.log("***************************************************************************")
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+const addTwo = (num1, num2) => ( num1 + num2 )
+
+console.log(addTwo(3, 4))
